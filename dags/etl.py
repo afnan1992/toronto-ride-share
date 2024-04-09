bd4162da0e82a7e9b4a8f197ac0644c45c79c701
afnan <afnan1992@github.com>
2024-04-09 18:02:42 -0400

    made changes to dag

--- dags/etl.py
+++ dags/etl.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
 import os
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 
 
 dag = DAG(
@@ -14,16 +15,16 @@ dag = DAG(
     # You can override them on a per-task basis during operator initialization
   
     description='ETL DAG',
-    start_date=datetime(2024, 3, 28),
+    start_date=datetime(2024, 4, 2),
     max_active_runs= 1
     
 ) 
 
-task_a = DockerOperator (
+ExtractRides = DockerOperator (
         dag = dag,
         task_id="extract_ride_share",
         image='afnan1992/toronto-ride-share:latest',
-        command='python extract.py',
+        command='python rides_extract.py',
         docker_url='tcp://docker-proxy:2375',
         #  mounts=[
         # Mount(source='/',
@@ -32,4 +33,35 @@ task_a = DockerOperator (
         # network_mode='host',
         
     )
-task_a 
+
+ExtractWeather = DockerOperator (
+        dag = dag,
+        task_id="extract_weather",
+        image='afnan1992/toronto-ride-share:latest',
+        command='python weather_extract.py',
+        docker_url='tcp://docker-proxy:2375',
+        #  mounts=[
+        # Mount(source='/',
+        #       target='/code',
+        #       type='bind'),],
+        # network_mode='host',
+        
+    )
+
+load_parquet = GCSToBigQueryOperator(
+    dag = dag,
+    task_id="gcs_to_biguqery",
+    bucket="toronto-ride-share-files",
+    source_objects=["*.parquet"],
+    destination_project_dataset_table=f"ride_share.rides",
+    autodetect = True,
+    source_format="PARQUET"
+    # schema_fields=[
+    #     {"name": "name", "type": "STRING", "mode": "NULLABLE"},
+    #     {"name": "post_abbr", "type": "STRING", "mode": "NULLABLE"},
+    # ],
+    # write_disposition="WRITE_TRUNCATE",
+)
+
+
+[ExtractRides,ExtractWeather] >> load_parquet 
